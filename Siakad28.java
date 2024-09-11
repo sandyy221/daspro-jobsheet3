7d517003016d5bb0c2936dc437b25b8761dbee52
sandyy221 <kurniawansandy808.com>
2024-09-11 23:29:21 +0700

    jobsheet3_sandy

---
+++ Siakad28.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Siakad28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nama, nim;
+        char kelas;
+        byte absen;
+        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+
+        System.out.print("Masukan nama: ");
+        nama = sc.nextLine();
+        System.out.print("Masukan NIM: ");
+        nim = sc.nextLine();
+        System.out.print("Masukan kelas: ");
+        kelas = sc.nextLine().charAt(0);
+        System.out.print("Masukan nomor absen: ");
+        absen = sc.nextByte();
+
+        System.out.print("Masukan nilai kuis: ");
+        nilaiKuis = sc.nextDouble();
+        System.out.print("Masukan nilai tugas: ");
+        nilaiTugas = sc.nextDouble();
+        System.out.print("Masukan nilai ujian: ");
+        nilaiUjian = sc.nextDouble();
+        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+        System.out.println("Nama: " + nama + " NIM: " + nim);
+        System.out.println("Kelas: " + kelas + "absen: " + absen);
+        System.out.println("Nilai Akhir: " + nilaiAkhir);
+
+            
+
+
+    }
+}
