438d8bb1f88d6fa71fe2d22242a851bb99fd82b0
sandyy221 <kurniawansandy808.com>
2024-09-12 15:06:45 +0700

    commitKafe28

--- Kafe28.java
+++ Kafe28.java
@@ -6,7 +6,7 @@ public class Kafe28 {
         boolean keanggotaan;
         int jmlKopi, jmlTeh, jmlRoti;
         double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0;
-        float diskon = 10 / 100f;
+        float diskon = 10 / 100;
 
         System.out.print("Masukan keanggotaan (true/false): ");
         keanggotaan = input.nextBoolean();
@@ -17,11 +17,15 @@ public class Kafe28 {
         System.out.print("Masukan jumlah pembelian roti: ");
         jmlRoti = input.nextInt();
         double totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
-        double nominalBayar = totalHarga - (diskon * totalHarga);
+        byte totalByte = (byte) totalHarga;
+        double nominalBayar = totalByte - (diskon * totalByte);
+        int nominalint = (int) nominalBayar;
 
         System.out.println("Keanggotaan pelangaan");
-        System.out.println("Item pembelian" + jmlKopi +"  kopi," + jmlTeh + " teh," + jmlRoti + " roti");
+        System.out.println("Item pembelian " + jmlKopi +"  kopi," + jmlTeh + " teh," + jmlRoti + " roti");
         System.out.println("Nominal bayar Rp " + nominalBayar);
+        System.out.println("Nominal bayar Rp " + nominalint );
+
 
 
     }
