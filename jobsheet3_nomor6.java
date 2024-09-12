44455e6411e17860a7165757b7f9dddef0b3d735
sandyy221 <kurniawansandy808.com>
2024-09-12 08:58:41 +0700

    jobshee3t_tugasNomor6

--- jobsheet3_nomor6.java
+++ jobsheet3_nomor6.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Siakad28 {
+public class jobsheet3_nomor6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String nama, nim;
